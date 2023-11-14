6e8b97933d745bff490feb37d73788521b542acc
Vitaly Juno <vvjuhno@gmail.com>
2023-11-14 23:13:10 +0200

    made an api request funnction in js

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import makeAPIRequest from './functions/makeAPIRequest';
 function App() {
-    const [joke, setJoke] = useState([""]);
-    useEffect(() => {
-        fetch("http://localhost/taskManegment/api/test.php")
-            .then((response) => response.text())
-            .then((response) => {
-                console.log(response);
-				setJoke(response);
-            })
-            .catch((error) => console.log(error));
-    }, []);
-    return (
-        <div>
-            <h2>Joke of the day:</h2>
-            {joke && <p>{joke}</p>}
-        </div>
-    );
+	const [test, setTest] = useState(['']);
+	useEffect(() => {
+		makeAPIRequest('test.php', res => {
+			if (res.error === undefined) {
+				console.log(res.message);
+				setTest(res.message);
+				return;
+			}
+			console.log(res.error);
+            setTest('something went wrong');
+		});
+	}, []);
+	return (
+		<div>
+			<h2>PHP + React test:</h2>
+			{test && <p>{test}</p>}
+		</div>
+	);
 }
 export default App;
