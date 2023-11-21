62892f53105a903c1cc1feb3c1b681957302b06d
KingRavo18 <144904011+KingRavo18@users.noreply.github.com>
2023-11-21 19:17:36 +0000

    f

---
+++ src/Signup.jsx
@@ -0,0 +1,22 @@
+import './style/Login.css'; // Import your stylesheet
+import Header from './components/Header.jsx';
+import React, { useState } from 'react';
+
+const Signup = () => {
+   
+  return (
+    <div>
+    <Header />
+    <div className="LoginDiv">
+      <input type="text" id="inputField" className="border-bottom-input" placeholder="Username"/>
+      <input type="text" id="inputField" className="border-bottom-input" placeholder="Password"/>
+      <input type="text" id="inputField" className="border-bottom-input" placeholder="Confirm Password"/>
+      <button className="login-button">
+      Login
+    </button>
+    </div>
+    </div>
+  );
+};
+
+export default Signup;
