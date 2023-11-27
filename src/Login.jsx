cfd6f17b63bf7b8c092b317da6078c5832fc4cd1
KingRavo18 <144904011+KingRavo18@users.noreply.github.com>
2023-11-27 13:07:17 +0000

    c

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,19 +1,26 @@
-import './style/Login.css'; // Import your stylesheet
-import Header from './components/Header.jsx';
-import React, { useState } from 'react';
+import React from 'react';
+import Header from './Header.jsx';
+import username_Logo from './assets/username_Logo.PNG';
+import password_Logo from './assets/password_Logo.PNG';
+import './style/Login.css';
 
 const Login = () => {
-   
   return (
     <div>
-    <Header />
-    <div className="LoginDiv">
-      <input type="text" id="inputField" className="border-bottom-input" placeholder="Username"/>
-      <input type="text" id="inputField" className="border-bottom-input" placeholder="Password"/>
-      <button className="login-button">
-      Login
-    </button>
-    </div>
+      <Header />
+      <div className="LoginDiv">
+        <div className="inputContainer">
+          <img src={username_Logo} alt="username_Logo" className="logo2" />
+          <input type="text" id="usernameInput" className="border-bottom-input" placeholder="Username" />
+        </div>
+        <div className="inputContainer">
+          <img src={password_Logo} alt="password_Logo" className="logo2" />
+          <input type="text" id="passwordInput" className="border-bottom-input" placeholder="Password" />
+        </div>
+        <button className="login-button">
+          Login
+        </button>
+      </div>
     </div>
   );
 };
--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,6 +1,9 @@
 import './style/Login.css'; // Import your stylesheet
-import Header from './components/Header.jsx';
-import React, { useState } from 'react';
+import Header from './Header.jsx';
+import username_Logo from './assets/username_Logo.PNG';
+import password_Logo from './assets/password_Logo.PNG';
+import confirm_Logo from './assets/confirm_Logo.PNG';
+import React from 'react';
 
 const Signup = () => {
    
@@ -8,9 +11,18 @@ const Signup = () => {
     <div>
     <Header />
     <div className="LoginDiv">
+    <div className="inputContainer">
+          <img src={username_Logo} alt="username_Logo" className="logo2" />
       <input type="text" id="inputField" className="border-bottom-input" placeholder="Username"/>
+    </div>
+    <div className="inputContainer">
+          <img src={password_Logo} alt="password_Logo" className="logo2" />
       <input type="text" id="inputField" className="border-bottom-input" placeholder="Password"/>
+    </div>
+    <div className="inputContainer">
+          <img src={confirm_Logo} alt="confirm_Logo" className="logo2" />
       <input type="text" id="inputField" className="border-bottom-input" placeholder="Confirm Password"/>
+    </div>
       <button className="login-button">
       Login
     </button>
@@ -20,3 +32,7 @@ const Signup = () => {
 };
 
 export default Signup;
+
+
+
+
