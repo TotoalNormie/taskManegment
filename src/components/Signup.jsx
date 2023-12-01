03ad785d38ab2b0c4b424dbaae091d7b2e62cdd2
Vitaly Juno <vvjuhno@gmail.com>
2023-12-01 18:33:56 +0200

    added login and signup routes

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,8 +1,8 @@
 import '../style/Login.css'; // Import your stylesheet
 import Header from './Header.jsx';
-import username_Logo from '../assets/username_Logo.PNG';
-import password_Logo from '../assets/password_Logo.PNG';
-import confirm_Logo from '../assets/confirm_Logo.PNG';
+import username_Logo from '../assets/username_Logo.png';
+import password_Logo from '../assets/password_Logo.png';
+import confirm_Logo from '../assets/confirm_Logo.png';
 import React from 'react';
 
 const Signup = () => {
