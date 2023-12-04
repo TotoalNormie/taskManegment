ad62d124b0abe5f104eb99bfc31f0c6ac0d2ca8a
Vitaly Juno <vvjuhno@gmail.com>
2023-12-04 20:26:42 +0200

    some changes idk< it works

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,16 +12,9 @@ const Sidebar = () => {
 		{ name: 'project 3', id: 3 },
 		{ name: 'project 4', id: 4 },
 	]);
-
-	// console.log(projects);
-
-	const [seen, setSeen] = useState(false);
-
-	const projectLinks = projects.map(( { name, id } ) => <li key={id}><Link to={'/?id=' + id}>{name}</Link></li>);
-	// projects.forEach(( { name, id } ) => console.log(name, id));
-
-
 	const location = useLocation();
+	const [seen, setSeen] = useState(false);
+	const projectLinks = projects.map(( { name, id } ) => <li className={('/' + id) == location.pathname ? 'active' : ''} key={id}><Link to={'/' + id}>{name}</Link></li>);
 	if (
 		location.pathname === '/login' ||
 		location.pathname === '/signup' ||
@@ -36,10 +29,10 @@ const Sidebar = () => {
 				<FontAwesomeIcon style={{transition: 'rotate .4s ease'}} className={seen ? 'flipped' : ''} icon={faAngleDown}></FontAwesomeIcon>
 				<span> Your Projects</span>
 			</button>
-			<div className={`projects hidden ${seen ? 'stretched' : ''}`}>
+			<ul className={`projects hidden ${seen ? 'stretched' : ''}`}>
 				{projectLinks}
 				<button>Add new project...</button>
-			</div>
+			</ul>
 		</nav>
 	);
 };
