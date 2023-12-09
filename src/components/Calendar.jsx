97b832e243941302fad15be4c91256c76fdc13c6
KingRavo18 <144904011+KingRavo18@users.noreply.github.com>
2023-12-09 20:58:04 +0000

    a

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,74 +1,130 @@
-import '../style/Calendar.css';
+// Import React and other necessary dependencies
 import React, { useState } from 'react';
+import '../style/Calendar.css';
 
+const DayLabels = () => {
+  // Array of day names to display
+  const dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+  return (
+    <div className="DayLabels">
+      {dayNames.map((day, index) => (
+        <div key={index} className="day-label">
+          {day}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 const Calendar = () => {
-  const [selectedValue, setSelectedValue] = useState(0);
+  const [selectedDate, setSelectedDate] = useState({
+    year: 2023,
+    month: 1,
+  });
+
   const [toggledButtons, setToggledButtons] = useState([]);
 
   const toggleButton = (index) => {
-    // Toggle the button at the specified index
     setToggledButtons((prevToggledButtons) => {
       const newToggledButtons = [...prevToggledButtons];
       newToggledButtons[index] = !newToggledButtons[index];
       return newToggledButtons;
     });
   };
+
   const generateDivs = () => {
     const divs = [];
-    for (let i = 0; i < selectedValue; i++) {
+    const daysInMonth = new Date(selectedDate.year, selectedDate.month, 0).getDate();
+    const firstDayOfMonth = new Date(selectedDate.year, selectedDate.month - 1, 1).getDay();
+  
+    // Calculate the offset to start the week with Sunday
+    const daysFromPrevMonth = (firstDayOfMonth - 1 + 7) % 7;
+  
+    // Days from the previous month
+    const prevMonthLastDay = new Date(selectedDate.year, selectedDate.month - 1, 0).getDate();
+    for (let i = daysFromPrevMonth; i > 0; i--) {
       divs.push(
-        <div key={i} className={`generated-div ${toggledButtons[i] ? 'active' : ''}`}>
-          <button onClick={() => toggleButton(i)}>Toggle</button>
-          <span>Div {i + 1}</span>
-        </div>
+        <button
+          key={`prev-${i}`}
+          className={`generated-div prev-next-month`}
+          disabled={true}
+        >
+          <span>{prevMonthLastDay - i + 1}</span>
+        </button>
       );
     }
+  
+    // Days from the current month
+    for (let i = 0; i < daysInMonth; i++) {
+      divs.push(
+        <button
+          key={`current-${i}`}
+          className={`generated-div ${toggledButtons[i] ? 'active' : ''}`}
+          onClick={() => toggleButton(i)}
+        >
+          <span>{i + 1}.</span>
+        </button>
+      );
+    }
+  
+    // Days from the next month
+    const totalDays = daysFromPrevMonth + daysInMonth;
+    const daysFromNextMonth = 42 - totalDays; // Total number of cells in a 6x7 grid
+  
+    for (let i = 0; i < daysFromNextMonth; i++) {
+      divs.push(
+        <button
+          key={`next-${i}`}
+          className={`generated-div prev-next-month`}
+          disabled={true}
+        >
+          <span>{i + 1}</span>
+        </button>
+      );
+    }
+  
     return divs;
   };
 
+  const handleMonthChange = (increment) => {
+    const newDate = { ...selectedDate };
+    newDate.month += increment;
+
+    if (newDate.month === 0) {
+      newDate.month = 12;
+      newDate.year -= 1;
+    } else if (newDate.month === 13) {
+      newDate.month = 1;
+      newDate.year += 1;
+    }
+
+    setSelectedDate(newDate);
+    setToggledButtons([]); // Reset toggled buttons when the month changes
+  };
+
   return (
-    <div className='ParentBox'>
-      <div className='YeMo'> 
-      <div></div>
-      <div className='Mo'><label className="Label" htmlFor="selectNumber">Month</label>
-      <select id="selectNumber" className='custom-select'
-        onChange={(e) => {
-          setSelectedValue(Number(e.target.value));
-          setToggledButtons([]); // Reset toggled buttons when the selection changes
-        }}
-        value={selectedValue}
-      >
-        <option value={0}> </option>
-        <option value={31}>January</option>
-        <option value={28}>February</option>
-        <option value={31}>March</option>
-        <option value={30}>April</option>
-        <option value={31}>May</option>
-        <option value={30}>June</option>
-        <option value={31}>July</option>
-        <option value={31}>August</option>
-        <option value={30}>September</option>
-        <option value={31}>October</option>
-        <option value={30}>November</option>
-        <option value={31}>December</option>
-      </select>
+    <div className="ParentBox">
+      <div className="YeMo">
+        <div>
+          <button onClick={() => handleMonthChange(-1)}>&lt;</button>
+        </div>
+        <label className="Label">
+          {new Date(selectedDate.year, selectedDate.month - 1, 1).toLocaleString('default', {
+            month: 'long',
+          })}{' '}
+          {selectedDate.year}
+        </label>
+        <div>
+          <button onClick={() => handleMonthChange(1)}>&gt;</button>
+        </div>
       </div>
-      </div>
-      <div className='Calendar'>
-
-      <div>
-     
-     
-
-      <div id="divContainer">
-        {selectedValue > 0 && generateDivs()}
+      <DayLabels /> {/* Add the new DayLabels component here */}
+      <div className="Calendar">
+        <div id="divContainer">{selectedDate.year > 0 && generateDivs()}</div>
       </div>
     </div>
-      
-    </div>
-</div>
-    
   );
 };
+
 export default Calendar;
