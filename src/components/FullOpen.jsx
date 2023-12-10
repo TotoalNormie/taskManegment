e239b201f5fe9f055417c6d759a76d7eac35d2b2
KingRavo18 <144904011+KingRavo18@users.noreply.github.com>
2023-12-10 23:01:00 +0000

    d

--- src/components/FullOpen.jsx
+++ src/components/FullOpen.jsx
@@ -1,19 +1,22 @@
 import '../style/FullOpen.css';
 import '../style/Calendar.css';
+import '../style/Tasks.css';
 import Calendar from './Calendar';
-import React, { useState } from 'react';
+import Tasks from './Tasks';
+import React from 'react';
 
 const FullOpen = () => {
   return (
-        <div className='ParentBox'>
-          <div className='Box'>
-            <p>penis</p>
-          </div>
-          <div className='Box'>
-            <Calendar />
-          </div>
-        </div>
+    <div className='ParentBox'>
+      <div className='BoxL'>
+      <Tasks />
+      </div>
+      <div className='Box'>
+        <Calendar />
+      </div>
+    </div>
   );
 };
 
 export default FullOpen;
+
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,14 +1,50 @@
 import '../style/Tasks.css';
-import React from 'react';
+import React, { useState } from 'react';
 
-const Tasks = () => {
+const ProjectSettingsPopup = ({ onClose }) => {
   return (
-    <div className='ParentBoxT'>
-      <div className='AddTask'></div>
-      <div className='ToDo'></div>
-      <div className='ToDo'></div>
-      <div className='ToDo'></div>
+    <div className="Popup">
+      <div className="PopupContent">
+        {/* Add your project settings content here */}
+        <p>Project Settings Popup</p>
+        <button onClick={onClose}>Close</button>
+      </div>
     </div>
   );
 };
+
+const Tasks = () => {
+  const [isPopupVisible, setPopupVisible] = useState(false);
+
+  const openPopup = () => {
+    setPopupVisible(true);
+  };
+
+  const closePopup = () => {
+    setPopupVisible(false);
+  };
+
+  return (
+    <div className="ParentBoxT">
+      <div className="AddTask">
+        <div className="AddAddTask">
+          <button className="Button" onClick={() => console.log('Task added')}>
+            Add Task
+          </button>
+        </div>
+        <div className="ProjectSettings">
+          <button className="Button2" onClick={openPopup}>
+            Project Settings
+          </button>
+        </div>
+      </div>
+      <div className="ToDo"></div>
+      <div className="ToDo"></div>
+      <div className="ToDo"></div>
+
+      {isPopupVisible && <ProjectSettingsPopup onClose={closePopup} />}
+    </div>
+  );
+};
+
 export default Tasks;
