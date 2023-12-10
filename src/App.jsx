3ff19e78cf83ffb8ac3936a33092ce48f03175a1
Vitaly Juno <vvjuhno@gmail.com>
2023-12-10 22:24:02 +0200

    changed paths a bit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Login from './components/Login';
 import Signup from './components/Signup';
-import Calendar from './components/Calendar';
 import './style/App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import APITest from './components/APITest';
@@ -21,11 +20,12 @@ function App() {
 				<Sidebar />
 				<main>
 					<Routes>
-						{/* <Route exact path='/' element={<APITest />}></Route> */}
-						<Route exact path='/:id?' element={<APITest />}></Route>
+						<Route exact path='/:id?' element={<FullOpen />}></Route>
 						<Route exact path='/test' element={<TestJomponent />}></Route>
 						<Route exact path='/signup' element={<Signup />}></Route>
 						<Route exact path='/login' element={<Login />}></Route>
+						<Route exact path='/home' element={<Home />}></Route>
+						<Route exact path='/fullopen' element={<FullOpen />}></Route>
 					</Routes>
 				</main>
 			</BrowserRouter>
