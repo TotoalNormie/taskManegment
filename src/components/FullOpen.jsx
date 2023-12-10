48550adb891106617d0d569f9657b4b53a93f890
KingRavo18 <144904011+KingRavo18@users.noreply.github.com>
2023-12-10 12:49:24 +0000

    g

--- src/components/FullOpen.jsx
+++ src/components/FullOpen.jsx
@@ -1,11 +1,23 @@
 import '../style/FullOpen.css';
-import React from 'react';
+import '../style/Calendar.css';
+import Calendar from './Calendar';
+import React, { useState } from 'react';
 
 const FullOpen = () => {
   return (
-    <div className='ParentBox'>
-      
+    <div>
+      <form>
+        <div className='ParentBox'>
+          <div className='Box'>
+            <p>penis</p>
+          </div>
+          <div className='Box'>
+            <Calendar />
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
+
 export default FullOpen;
