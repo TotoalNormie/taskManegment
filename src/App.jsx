6e8ddbc21ffd82c5b6db4554edde4a848cb0f5ac
KingRavo18 <144904011+KingRavo18@users.noreply.github.com>
2023-12-10 20:27:12 +0000

    ;

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react';
 import makeAPIRequest from './functions/makeAPIRequest';
 import FullOpen from './components/FullOpen';
 import Home from './components/Home';
@@ -6,31 +5,31 @@ import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Login from './components/Login';
 import Signup from './components/Signup';
-import Tasks from './components/Tasks';
 import './style/App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import APITest from './components/APITest';
 import TestJomponent from './components/TestJomponent';
+import { UserProvider } from './components/UserProvider';
 
 function App() {
 	console.log(document.cookie);
 	return (
-		<BrowserRouter>
-			<Header />
-			<Sidebar />
-			<main>
-				<Routes>
-					{/* <Route exact path='/' element={<APITest />}></Route> */}
-					<Route exact path='/:id?' element={<APITest />}></Route>
-					<Route exact path='/test' element={<TestJomponent />}></Route>
-					<Route exact path='/signup' element={<Signup />}></Route>
-					<Route exact path='/login' element={<Login />}></Route>
-					<Route exact path='/home' element={<Home />}></Route>
-					<Route exact path='/fullopen' element={<FullOpen />}></Route>
-					<Route exact path='/tasks' element={<Tasks />}></Route>
-				</Routes>
-			</main>
-		</BrowserRouter>
+		<UserProvider>
+			<BrowserRouter>
+				<Header />
+				<Sidebar />
+				<main>
+					<Routes>
+						<Route exact path='/:id?' element={<FullOpen />}></Route>
+						<Route exact path='/test' element={<TestJomponent />}></Route>
+						<Route exact path='/signup' element={<Signup />}></Route>
+						<Route exact path='/login' element={<Login />}></Route>
+						<Route exact path='/home' element={<Home />}></Route>
+						<Route exact path='/fullopen' element={<FullOpen />}></Route>
+					</Routes>
+				</main>
+			</BrowserRouter>
+		</UserProvider>
 	);
 }
 export default App;
